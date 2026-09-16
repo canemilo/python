8b350fc94cff5c49e735335ffbc00726f5ac7370
DAM2 <dannigarrido@hotmail.es>
2026-09-16 17:28:47 +0200

    ejercicio 1 python

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+// 1
+
+/// py -3.14 -m venv entorno_dani
+
+// 2
+
+
+/// .\entorno_dani\Scripts\activate
+
+// 3
+
+/// pip list
+
+
+// 4
+
+/// pip install pandas
+
+
+// 5
+
+/// pip list
+
+// 6
+
+/// deactivate
+
+// 7
+
+/// pip list
+
+
+// 8
+
+/// .\entorno_dani\Scripts\activate
+
+// 9
+
+/// pip uninstall pandas
+
+
+// 10
+
+/// pip list
